03486f29bf1eff04fb51d713289f71188b4aed96
futug <102650930+futug@users.noreply.github.com>
2023-08-29 17:25:15 +0600

    longread button js

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,8 +2,6 @@ console.log("components");
 import "./components/validation";
 import "./components/ex";
 import "./components/preloader";
-
 import "./components/scrollToTop";
-
-import "./components/navbar-selector";
-
+// import "./components/navbar-selector";
+import "./components/blogShowMore";
