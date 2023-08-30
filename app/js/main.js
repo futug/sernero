2bf1b7deb6c21e7c020aa6c15928eedca0914e2a
sergykt <sergykt1992@mail.ru>
2023-08-30 19:00:20 +0600

    fixed horizontal form style

--- app/js/main.js
+++ app/js/main.js
@@ -262,8 +262,8 @@ if (block1 && block2) {
 
 const servicePaths = ['google-ads', 'google-search-ads', 'video-marketing', 'google-analytics', 'audit', 'landing-page', 'seo'];
 const path = window.location.pathname;
-const paths = path.split('/');
-const lastPath = paths[paths.length - 1]?.replace(/\.html$/, "");
+const paths = path.split('/').filter(path => path);
+let lastPath = paths[paths.length - 1]?.replace(/\.html$/, "");
 let activeLink = document.querySelector(`[data-link="${lastPath}"]`);
 if (servicePaths.includes(lastPath)) {
   activeLink = document.querySelector('[data-link="services"]');
