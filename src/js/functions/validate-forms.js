1c0c6ba97df49323eeeadafd9ac6b6df1295eded
sergykt <sergykt1992@mail.ru>
2023-08-30 15:29:52 +0600

    fixed validator, fixed footer(added links), fixed style google-ads

--- src/js/functions/validate-forms.js
+++ src/js/functions/validate-forms.js
@@ -1,65 +1,62 @@
 import JustValidate from "just-validate";
-import Inputmask from "inputmask";
+//import Inputmask from "inputmask";
 
 export const validateForms = (selector, rules, afterSend) => {
-  const form = document?.querySelector(selector);
-  const telSelector = form?.querySelector('input[type="tel"]');
-
-  if (!form) {
-    // console.error("Нет такого селектора!");
-    return false;
-  }
+  const forms = document?.querySelectorAll(selector);
 
   if (!rules) {
     console.error("Вы не передали правила валидации!");
     return false;
   }
 
-  if (telSelector) {
-    const inputMask = new Inputmask("+9 (999) 999-99-99");
-    inputMask.mask(telSelector);
+  forms.forEach((form) => {
+    /*if (telSelector) {
+      const inputMask = new Inputmask("+9 (999) 999-99-99");
+      inputMask.mask(telSelector);
+
+      for (let item of rules) {
+        if (item.tel) {
+          item.rules.push({
+            rule: "function",
+            validator: function () {
+              const phone = telSelector.inputmask.unmaskedvalue();
+              return phone.length > 8;
+            },
+            errorMessage: item.telError,
+          });
+        }
+      }
+    } */
+    const id = form.id;
+
+    const validation = new JustValidate(`#${id}`);
 
     for (let item of rules) {
-      if (item.tel) {
-        item.rules.push({
-          rule: "function",
-          validator: function () {
-            const phone = telSelector.inputmask.unmaskedvalue();
-            return phone.length === 11;
-          },
-          errorMessage: item.telError,
-        });
-      }
+      validation.addField(item.ruleSelector, item.rules);
     }
-  }
 
-  const validation = new JustValidate(selector);
+    validation.onSuccess((ev) => {
+      let formData = new FormData(ev.target);
 
-  for (let item of rules) {
-    validation.addField(item.ruleSelector, item.rules);
-  }
+      // let xhr = new XMLHttpRequest();
 
-  validation.onSuccess((ev) => {
-    let formData = new FormData(ev.target);
+      // xhr.onreadystatechange = function () {
+      //   if (xhr.readyState === 4) {
+      //     if (xhr.status === 200) {
+      //       if (afterSend) {
+      //         afterSend();
+      //       }
+      //       console.log("Отправлено");
+      //     }
+      //   }
+      // };
 
-    // let xhr = new XMLHttpRequest();
+      // xhr.open("POST", "mail.php", true);
+      // xhr.send(formData);
 
-    // xhr.onreadystatechange = function () {
-    //   if (xhr.readyState === 4) {
-    //     if (xhr.status === 200) {
-    //       if (afterSend) {
-    //         afterSend();
-    //       }
-    //       console.log("Отправлено");
-    //     }
-    //   }
-    // };
+      console.log(formData);
 
-    // xhr.open("POST", "mail.php", true);
-    // xhr.send(formData);
-
-    console.log(formData);
-
-    ev.target.reset();
+      ev.target.reset();
+    });
   });
 };
