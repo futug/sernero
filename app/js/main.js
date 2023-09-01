ffb1e32f42c22573eb7148d9b8e5e855e285fb88
sergykt <sergykt1992@mail.ru>
2023-09-01 16:34:00 +0600

    fixed lightbox, header

--- app/js/main.js
+++ app/js/main.js
@@ -387,29 +387,34 @@ if (portfolioList) {
 /***/ (() => {
 
 const gallery = document.querySelectorAll('[data-lightbox="gallery"]');
-const overlay = document.querySelector('.overlay');
-const lightBox = document.querySelector('.lightbox');
-const closeButton = document.querySelector('.close-button');
-gallery.forEach(item => {
-  item.addEventListener('click', e => {
-    e.stopPropagation();
-    const src = item.getAttribute('src');
-    const img = document.createElement('img');
-    img.src = src;
-    img.className = 'lightbox__image';
-    lightBox.append(img);
-    overlay.classList.toggle('overlay_active');
-    document.body.style.overflow = 'hidden';
+if (gallery.length > 0) {
+  const overlay = document.querySelector('.overlay');
+  const lightBox = document.querySelector('.lightbox');
+  const img = document.querySelector('.lightbox__image');
+  const closeButton = document.querySelector('.close-button');
+  gallery.forEach(item => {
+    item.addEventListener('click', e => {
+      e.stopPropagation();
+      const src = item.getAttribute('src');
+      img.src = src;
+      overlay.classList.toggle('overlay_active');
+      document.body.style.overflow = 'hidden';
+    });
   });
-});
-overlay?.addEventListener('click', e => {
-  e.stopPropagation();
-  if (e.target.className !== 'lightbox__image') {
-    overlay.classList.toggle('overlay_active');
-    document.body.style.overflow = 'auto';
-    lightBox?.replaceChildren();
-  }
-});
+  overlay.addEventListener('click', e => {
+    e.stopPropagation();
+    if (e.target.className !== 'lightbox__image') {
+      overlay.classList.remove('overlay_active');
+      document.body.style.overflow = 'auto';
+    }
+  });
+  document.body.addEventListener("keydown", e => {
+    if (e.code == "Escape" && overlay.classList.contains('overlay_active')) {
+      overlay.classList.remove('overlay_active');
+      document.body.style.overflow = 'auto';
+    }
+  });
+}
 
 /***/ }),
 
